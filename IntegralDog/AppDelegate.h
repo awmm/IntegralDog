e35c8d84c0902ad73b5e4b234d3d83802bddaf07
hanenwm <1448463733@qq.com>
2016-01-21 15:09:59 +0800

    first

---
+++ IntegralDog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntegralDog
+//
+//  Created by wmm on 16/1/21.
+//  Copyright © 2016年 hanen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntegralDog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntegralDog
+//
+//  Created by wmm on 16/1/21.
+//  Copyright © 2016年 hanen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
